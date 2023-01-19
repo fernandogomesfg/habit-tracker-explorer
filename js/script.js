242f4161bfe0a05a82b16368ab924ac4508b85f4
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2023-01-19 14:08:10 +0200

    chore: deixando o header fixo
    O header fixo; acrecentar mais alguns habitos para poder testar o espacamento da pagina

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
 
 const data = {
-    run: ['01-01', '01-06'],
+    run: ['01-01', '01-06', '01-07', '01-08', '01-09', '01-10',],
     water: ['01-02', '01-06'],
     food: ['01-01', '01-02'],
-    dance: ['01-01', '01-03']
+    dance: ['01-01', '01-03'],
+    coding: ['01-01', '01-03', '01-08', '01-09']
 }
 
 nlwSetup.setData(data);
