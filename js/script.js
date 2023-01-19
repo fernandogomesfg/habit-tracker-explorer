b598c6ab79215a5e833b95363d184602397ce1c1
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2023-01-19 13:34:31 +0200

    chore: implementacao da biblioteca

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const from = document.querySelector("from");
-const nlwSetup = new NLWSetup(from);
+const form = document.querySelector("form");
+const nlwSetup = new NLWSetup(form);
+
+const data = {
+    run: ['01-01', '01-06'],
+    water: ['01-02', '01-06'],
+    food: ['01-01', '01-02'],
+    dance: ['01-01', '01-03']
+}
+
+nlwSetup.setData(data);
+nlwSetup.load();
