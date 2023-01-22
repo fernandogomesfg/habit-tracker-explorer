815238607940550ad828bbc91513bbeaab0cd0bd
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2023-01-22 20:23:02 +0200

    chore: alteracao do noe da chave na funcao localStorage

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ function add() {
 }
 
 function save() {
-    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+    localStorage.setItem("NLWSetup", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup")) || {}
 nlwSetup.setData(data);
 nlwSetup.load();
