dbe2ec825adc8ab500c2e64c3b1efc10c134efad
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2023-01-22 19:42:49 +0200

    chore: funcao para adicionar dia a lista

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-06', '01-07', '01-08', '01-09', '01-10',],
-    water: ['01-02', '01-06'],
-    food: ['01-01', '01-02'],
-    dance: ['01-01', '01-03'],
-    coding: ['01-01', '01-03', '01-08', '01-09']
+button.addEventListener('click', add);
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert("Dia ja incluso 🔴");
+        return;
+    }
+
+    alert("Dia adicionado com sucesso ✅");
+    nlwSetup.addDay(today);
 }
 
-nlwSetup.setData(data);
-nlwSetup.load();
+// const data = {
+//     run: ['01-01', '01-06', '01-07', '01-08', '01-09', '01-10',],
+//     water: ['01-02', '01-06'],
+//     food: ['01-01', '01-02'],
+//     dance: ['01-01', '01-03'],
+//     coding: ['01-01', '01-03', '01-08', '01-09']
+// }
+
+// nlwSetup.setData(data);
+// nlwSetup.load();
